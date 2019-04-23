3b8c966ac5264e980f9f8413d65bf36b9c9ef07e
marshaps <marshaps@LCBLAB209-22.uccob.uc.edu>
2019-04-23 09:12:01 -0400

    Changed enumCondition to private to access in Main

--- AvocadoSimulator01/src/main/Main.java
+++ AvocadoSimulator01/src/main/Main.java
@@ -3,17 +3,26 @@
  * nicholdw@ucmail.uc.edu
  */
 package main;
+
+import avocado.Avocado;
+
 /***
  * Exercise our Avocado classes
  * @author nicomp
  *
  */
 public class Main {
-	enum myEnum {A,B,C,D};	// Demo of enumerated date type
+//	enum myEnum {A,B,C,D};	// Demo of enumerated date type
 
 	public static void main(String[] args) {
-		myEnum e = myEnum.A;
-		System.out.println(e.toString());
+		
+		// Declare and instantiation an avocado
+		Avocado bob = new Avocado("Purple", Avocado.enumCondition.squishy, 4);
+		
+		
+		
+//		myEnum e = myEnum.A;
+//		System.out.println(e.toString());
 
 	}
 }
